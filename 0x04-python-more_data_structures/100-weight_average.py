e28d5c426e316c72a3b3ccf5af09bd628ebf1011
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-05-07 18:06:10 +0000

    Weighted average!

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is None and type(my_list) == list:
+    if not my_list:
         return 0
     s, w = 0, 0
     for score, weight in my_list:
