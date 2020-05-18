ad89772260ccb4ecdc7e924293cb9e4fc27fc292
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-05-18 11:13:41 +0000

    Divide a list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     liste = []
+    result = 0
     for i in range(list_length):
+        result = 0
         try:
             result = my_list_1[i] / my_list_2[i]
         except IndexError:
