92094272237d5a578d19e7f657ca48265c20d746
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-05-28 12:39:54 +0000

    Detect instance deletion

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""Rectangle class."""
+
+
+class Rectangle:
+    """class rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize Rectangle.
+        Args:
+            width (int): width
+            height (int): height.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get the width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setters"""
+        if type (value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter"""
+        if type (value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return area """
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """Rectangle perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        """print with #"""
+        ch = ""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        ch += "\n".join("#" * self.__width
+                         for j in range(self.__height))
+        return ch
+
+    def __repr__(self):
+        """representation Rectangle."""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+    def __del__(self):
+        """bye if deleted"""
+        print("Bye rectangle...")
