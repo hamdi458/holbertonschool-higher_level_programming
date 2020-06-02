a39c23b1168651a42e4d75d599c739edf2bca293
hamdi458 <hamdi.ghorbel78@gmail.com>
2020-06-02 20:54:46 +0000

    My integer

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""class MyInt"""
+
+
+class MyInt(int):
+    """class MyInt that inherits from int"""
+
+    def __eq__(self, value):
+        """return true or false"""
+        return self.real != value
+
+    def __ne__(self, value):
+        """return true or false"""
+        return int(self) == other
