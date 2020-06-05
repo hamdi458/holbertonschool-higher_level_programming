5d3563fb92df6c85297210b758438bfe4be9c252
hamdi458 <hamdi.ghorbel78@gmail.com>
2020-06-05 17:43:29 +0000

    json

---
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function that returns the JSON representation of an object (string)"""
+
+
+def to_json_string(my_str):
+    """function that returns the JSON representation of an object (string)"""
+    import json
+    y = json.loads(my_str)
+    return y
