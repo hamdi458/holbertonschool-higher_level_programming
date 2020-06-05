ccbac15bb8932fc0e29183f188d9a89352813238
hamdi458 <hamdi.ghorbel78@gmail.com>
2020-06-05 17:40:14 +0000

    json

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""to_json_string"""
-
-import json
-
+"""function that returns the JSON representation of an object (string)"""
 
 def to_json_string(my_obj):
-    """JSON format"""
+    """function that returns the JSON representation of an object (string)"""
+    import json
     y = json.dumps(my_obj)
     return y
