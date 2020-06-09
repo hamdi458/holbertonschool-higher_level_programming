a3041ffe4bd1989201fd201fb76ffc35e2e80b15
hamdi458 <hamdi.ghorbel78@gmail.com>
2020-06-09 16:08:09 +0000

    json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -13,3 +13,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """ returns the JSON string representation of list_dictionaries"""
+        import json
+        if list_dictionaries == None or  list_dictionaries == []:
+            return []
+        y = json.dumps(list_dictionaries)
+        return y
