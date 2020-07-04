aad3776a709c86f5ce1e4efb546349206d5a66e5
hamdi458 <hamdi.ghorbel78@gmail.com>
2020-07-04 07:42:37 +0000

    wiw

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -31,7 +31,42 @@ class Node:
     @next_node.setter
     def next_node(self, value):
         """wiw"""
-        if value is not None and value is not Node:
+        if value is not None and type(value) is not Node:
             raise TypeError("next_node must be a Node object")
         else:
             self.__next_node = value
+
+
+class SinglyLinkedList:
+    """Define class_ """
+    def __init__(self):
+        """cons """
+        self.__head = None
+
+    def __str__(self):
+        """str """
+        string = ""
+        tmp = self.__head
+        while tmp is not None:
+            string += str(tmp.data)
+            string += "\n"
+            tmp = tmp.next_node
+        string = string[:-1]
+        return string
+
+    def sorted_insert(self, value):
+        """sorted list"""
+        new_node = Node(value)
+        if self.__head is None:
+            new_node.next_node = None
+            self.__head = new_node
+        elif self.__head.data > value:
+            new_node.next_node = self.__head
+            self.__head = new_node
+        else:
+            tmp = self.__head
+            while (tmp.next_node is not None and
+                    tmp.next_node.data < value):
+                tmp = tmp.next_node
+            new_node.next_node = tmp.next_node
+            tmp.next_node = new_node
