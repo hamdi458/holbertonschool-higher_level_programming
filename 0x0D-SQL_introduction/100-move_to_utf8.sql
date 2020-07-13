d17215cf334a84545fc32e126009c4699140cefe
hamdi458 <hamdi.ghorbel78@gmail.com>
2020-07-13 20:24:38 +0000

    sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,4 @@
 -- Convert the database:
 USE hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-ALTER TABLE first_table ROW_FORMAT=DYNAMIC;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
