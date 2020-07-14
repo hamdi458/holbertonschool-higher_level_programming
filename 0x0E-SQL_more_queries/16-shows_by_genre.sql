8934de462c22b2f524bde8c9f511da46dece5d2b
hamdi458 <hamdi.ghorbel78@gmail.com>
2020-07-14 21:17:31 +0000

    sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,4 @@
 -- script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
 SELECT ts.title, ge.name
-FROM tv_shows ts
-LEFT JOIN tv_show_genres s ON ts.id = s.show_id LEFT JOIN tv_genres ge ON s.genre_id = ge.id
+FROM tv_shows AS ts LEFT JOIN tv_show_genres AS s ON ts.id = s.show_id LEFT JOIN tv_genres AS ge ON s.genre_id = ge.id
 ORDER BY ge.name, ts.title;
