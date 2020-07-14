e02f25234dd231e30ff844ab734a72b4febe6ba1
hamdi458 <hamdi.ghorbel78@gmail.com>
2020-07-14 21:10:01 +0000

    sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,5 @@
 -- script that lists all Comedy shows in the database hbtn_0d_tvshows.
 SELECT tv_shows.title FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id LEFT JOIN tv_genres ON tv_genres.idd = tv_show_genres.genre_id
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_genres.name = "Comedy"
 ORDER BY tv_shows.title;
