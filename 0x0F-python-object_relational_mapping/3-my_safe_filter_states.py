40fd69bcb2b01bb824930b7b45caadbb152f836b
hamdi458 <hamdi.645@gmail.com>
2020-08-09 21:25:20 +0100

    sql

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" script that takes in an argument and displays all values """
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    cnx = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                          passwd=argv[2], db=argv[3])
+    cursor = cnx.cursor()
+    cursor.execute(
+        "select * FROM states order by id")
+    lines = cursor.fetchall()
+    for i in lines:
+        if (i[1] == argv[4]):
+            print(i)
+    cursor.close()
+    cnx.close()
