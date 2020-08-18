e0e21d9cc49bda7e3c5376f032562cea0148c565
hamdi458 <hamdi.645@gmail.com>
2020-08-18 15:29:06 +0100

    wiw

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-curl -sI "$1" | grep [Cc]ontent-[Ll]ength: | awk '{ print $2 }'
+# bash script
+curl --head --silent --location "$URL" | grep -i "content-length:" | tr -d " \t" | cut -d ':' -f 2
