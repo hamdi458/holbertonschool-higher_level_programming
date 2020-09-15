c5b50a3e1daadd8638f08f9c98e69a39f6ef0f3c
hamdi458 <hamdi.645@gmail.com>
2020-09-15 12:41:14 +0100

    fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-if (process.argv.length > 2) {
-  console.log('Arguments found');
-} else {
+const l = process.argv.length;
+if (l === 2) {
   console.log('No argument');
+} else if (l === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
