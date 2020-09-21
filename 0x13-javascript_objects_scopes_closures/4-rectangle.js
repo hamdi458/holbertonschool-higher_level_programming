152014f8ee85dd5f2e939cafad26893195bfdee9
hamdi458 <hamdi.645@gmail.com>
2020-09-21 16:02:07 +0100

    rect

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -14,8 +14,9 @@ module.exports = class Rectangle {
   }
 
   rotate () {
+    let a = this.height;
     this.height = this.width;
-    this.width = this.height;
+    this.width = a;
   }
 
   double () {
