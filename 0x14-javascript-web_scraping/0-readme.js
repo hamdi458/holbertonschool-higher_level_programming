09070aa9d0b622742c7e3c79265ec44ffcb316ca
hamdi458 <hamdi.645@gmail.com>
2020-09-22 15:28:23 +0100

    read_write_file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (err1, data) {
-  if (err1) throw err1;
-  else console.log(data);
+  console.log(error || content);
 });
